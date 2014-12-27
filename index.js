139c7b7d79b9a24607f9cd6d1bbf43b13cfb9525
natew <natewienert@gmail.com>
2014-12-26 17:04:43 -0700

    Testing out exporting everything individually from here

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 require('reapp-object-assign');
 
 module.exports = {
-  component: require('reapp-component'),
-  batching: require('reapp-raf-batching'),
-  reducer: require('reapp-reducer'),
-  request: require('reapp-request'),
-  routes: require('reapp-routes'),
-  server: require('reapp-server'),
-  webpackServer: require('reapp-server/webpack/server'),
-  ui: require('reapp-ui'),
-  utils: require('reapp-utils')
+  run: require('./src/run'),
+  env: require('./src/env'),
+  helpers: require('./src/helpers/helpers'),
+  mixins: require('./src/mixins/mixins')
 };
