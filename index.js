54281c79eef7f1bed33a81140d7f4056eab99ad1
natew <natewienert@gmail.com>
2014-12-29 11:23:34 -0800

    Move run to reapp-routes, update package, add initTouch

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var React = require('react');
+
 require('reapp-object-assign');
 
+// this won't be necessary after React 1.0
+function initTouch() {
+  React.initializeTouchEvents(true);
+  var InjectTapEventPlugin = require('react-tap-event-plugin');
+  var isTouchDevice = 'ontouchstart' in document.documentElement;
+  if (isTouchDevice)
+    InjectTapEventPlugin();
+}
+
 module.exports = {
-  run: require('./src/run'),
+  initTouch,
   env: require('./src/env'),
   helpers: require('./src/helpers/helpers'),
   mixins: require('./src/mixins/mixins')
